4428d4b43c594f9da1575387752f4b6aede5d141
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-01-08 21:21:47 +0700

    prepare boostrap

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.module.css";
+
+function index() {
+  return <div>index</div>;
+}
+
+export default index;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.module.css";
+
+function index() {
+  return <div></div>;
+}
+
+export default index;
